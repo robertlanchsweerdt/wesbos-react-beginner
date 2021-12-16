d2addcec132f4e70e7da0f0486f699909605af1d
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-12-16 16:13:46 -0500

    use of map method to display inventory

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 export default class App extends Component {
   // using state as a property
@@ -31,6 +32,12 @@ export default class App extends Component {
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market' />
+          <ul className='fishes'>
+            {Object.keys(this.state.fishes).map((key) => {
+              console.log(key);
+              return <Fish key={key} fish={this.state.fishes[key]} />;
+            })}
+          </ul>
         </div>
         <Order />
         <Inventory
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
+
+export default class Fish extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    const { desc, image, name, price, status } = this.props.fish;
+
+    return (
+      <li className='menu-fish'>
+        <img src={image} alt={name} />
+        <h3 className='fish-name'>
+          {name}
+          <span className='price'>{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <p>{status}</p>
+        <button disabled={!status}>
+          {status ? 'Add to cart' : 'Sold out'}
+        </button>
+      </li>
+    );
+  }
+}
